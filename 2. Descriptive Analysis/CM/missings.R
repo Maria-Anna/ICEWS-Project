9a85875086a3f5d2be3d6c43df920af0f103cf5f
Clara <strasserceballosclara@gmail.com>
2022-08-10 15:42:08 +0200

    new changes

--- 2. Descriptive Analysis/CM/missings.R
+++ 2. Descriptive Analysis/CM/missings.R
@@ -4,9 +4,11 @@
 setwd("~/Desktop/Consulting Bewaffnete Konflikte/Datasets_Africa")
 
 #Load events africa data set
-events_africa<- read.delim("events_africa_no_israel.tsv",header = TRUE,sep= "\t")
+load("/Users/clarita/Desktop/Consulting Bewaffnete Konflikte/Datasets_Africa/Data Sets/data_icews_cm.Rdata")
+min(data_icews_cm$Year_month)
+events_africa<- read.delim("events_africa.tsv",header = TRUE,sep= "\t")
 events_africa$Year <- format(as.Date(events_africa$Event.Date, format="%Y-%m-%d"),"%Y") 
-
+events_africa<-data_icews_cm
 ###############################
 #Missings Summary Statistics
 ##############################
--- 2. Descriptive Analysis/Escalation Covariates/The_Dynamic_Process.R
+++ 2. Descriptive Analysis/Escalation Covariates/The_Dynamic_Process.R
@@ -181,9 +181,142 @@ ggsave(b, filename = "/Users/clarita/Desktop/Consulting\ Bewaffnete\ Konflikte/D
 ggsave(c, filename = "/Users/clarita/Desktop/Consulting\ Bewaffnete\ Konflikte/Descriptives\ and\ Plots/Plots/Escalation\ Plots/Dynamics/dynamic_egypt_2.png")
 
 
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+#######################
+#Plot: Egypt 2001-2012
+########################
+
+#Plot for each Escalation Covariate the Evolution of the absolute frequency over the months, with lines telling whether state-based fatality in CM or not
+
+cm_egy<-cm_data %>% filter(country_name=="Egypt",year %in% (2013:2016))
+
+a<-ggplot(cm_egy, aes(year_month,reb_gov_low_level))+ geom_point()+ geom_line(aes(group=1))+
+  geom_vline(aes(xintercept = year_month), data = ~ filter(cm_data,country_name=="Egypt",year %in% (2013:2016),ged_dummy_sb=="TRUE"),linetype="dotted")+
+  xlab("Year")+ ylab("Freq")+
+  ggtitle("Reb-Gov Low Level Violence")+
+    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(plot.title = element_text(color = "black", size=14, hjust=0.5),
+          panel.background = element_blank(),
+          axis.line = element_line(colour = "black"),
+          axis.title.x = element_text(hjust=0.5, size=16),
+          axis.title.y= element_text(hjust=0.5,size=16),
+          axis.text = element_text(size=12,colour = "black")) +
+  scale_x_discrete(breaks = unique(cm_egy$year_month)[seq(1,48,6)])
+
+b<-ggplot(cm_egy, aes(year_month,gov_reb_low_level))+ geom_point()+ geom_line(aes(group=1))+
+  geom_vline(aes(xintercept = year_month), data = ~ filter(cm_data,country_name=="Egypt",year %in% (2013:2016),ged_dummy_sb=="TRUE"),linetype="dotted")+
+  xlab("Year")+ ylab("Freq")+
+  ggtitle("Gov-Reb Low Level Violence")+
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(plot.title = element_text(color = "black", size=14, hjust=0.5),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black"),
+        axis.title.x = element_text(hjust=0.5, size=16),
+        axis.title.y= element_text(hjust=0.5,size=16),
+        axis.text = element_text(size=12,colour = "black")) +
+  scale_x_discrete(breaks = unique(cm_egy$year_month)[seq(1,48,6)])
+
+c<-ggplot(cm_egy, aes(year_month,opp_gov_low_level))+ geom_point()+ geom_line(aes(group=1))+
+  geom_vline(aes(xintercept = year_month), data = ~ filter(cm_data,country_name=="Egypt",year %in% (2013:2016),ged_dummy_sb=="TRUE"),linetype="dotted")+
+  xlab("Year")+ ylab("Freq")+
+  ggtitle("Opp-Gov Low Level Violence")+
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(plot.title = element_text(color = "black", size=14, hjust=0.5),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black"),
+        axis.title.x = element_text(hjust=0.5, size=16),
+        axis.title.y= element_text(hjust=0.5,size=16),
+        axis.text = element_text(size=12,colour = "black")) +
+  scale_x_discrete(breaks = unique(cm_egy$year_month)[seq(1,48,6)])
+
+d<-ggplot(cm_egy, aes(year_month,gov_opp_low_level))+ geom_point()+ geom_line(aes(group=1))+
+  geom_vline(aes(xintercept = year_month), data = ~ filter(cm_data,country_name=="Egypt",year %in% (2013:2016),ged_dummy_sb=="TRUE"),linetype="dotted")+
+  xlab("Year")+ ylab("Freq")+
+  ggtitle("Gov-Opp Low Level Violence")+
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(plot.title = element_text(color = "black", size=14, hjust=0.5),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black"),
+        axis.title.x = element_text(hjust=0.5, size=16),
+        axis.title.y= element_text(hjust=0.5,size=16),
+        axis.text = element_text(size=12,colour = "black")) +
+  scale_x_discrete(breaks = unique(cm_egy$year_month)[seq(1,48,6)])
 
 
 
+e<-ggplot(cm_egy, aes(year_month,gov_opp_accommodations))+ geom_point()+ geom_line(aes(group=1))+
+  geom_vline(aes(xintercept = year_month), data = ~ filter(cm_data,country_name=="Egypt",year %in% (2013:2016),ged_dummy_sb=="TRUE"),linetype="dotted")+
+  xlab("Year")+ ylab("Freq")+
+  ggtitle("Gov-Opp Accommodations")+
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(plot.title = element_text(color = "black", size=14, hjust=0.5),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black"),
+        axis.title.x = element_text(hjust=0.5, size=16),
+        axis.title.y= element_text(hjust=0.5,size=16),
+        axis.text = element_text(size=12,colour = "black")) +
+  scale_x_discrete(breaks = unique(cm_egy$year_month)[seq(1,48,6)])
+
+f<-ggplot(cm_egy, aes(year_month,gov_reb_accommodations))+ geom_point()+ geom_line(aes(group=1))+
+  geom_vline(aes(xintercept = year_month), data = ~ filter(cm_data,country_name=="Egypt",year %in% (2013:2016),ged_dummy_sb=="TRUE"),linetype="dotted")+
+  xlab("Year")+ ylab("Freq")+
+  ggtitle("Gov-Reb Accommodations")+
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(plot.title = element_text(color = "black", size=14, hjust=0.5),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black"),
+        axis.title.x = element_text(hjust=0.5, size=16),
+        axis.title.y= element_text(hjust=0.5,size=16),
+        axis.text = element_text(size=12,colour = "black")) +
+  scale_x_discrete(breaks = unique(cm_egy$year_month)[seq(1,48,6)])+
+  scale_y_continuous(limits = c(0, 6), breaks = c(0,2,4,6))
+
+g<-ggplot(cm_egy, aes(year_month,gov_opp_nonviol_repression))+ geom_point()+ geom_line(aes(group=1))+
+  geom_vline(aes(xintercept = year_month), data = ~ filter(cm_data,country_name=="Egypt",year %in% (2013:2016),ged_dummy_sb=="TRUE"),linetype="dotted")+
+  xlab("Year")+ ylab("Freq")+
+  ggtitle("Gov-Opp Non-Violent Repression")+
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(plot.title = element_text(color = "black", size=14, hjust=0.5),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black"),
+        axis.title.x = element_text(hjust=0.5, size=16),
+        axis.title.y= element_text(hjust=0.5,size=16),
+        axis.text = element_text(size=12,colour = "black")) +
+  scale_x_discrete(breaks = unique(cm_egy$year_month)[seq(1,48,6)])+
+  scale_y_continuous(limits = c(0, 3), breaks = c(0,1,2,3))
+
+
+h<-ggplot(cm_egy, aes(year_month,gov_reb_nonviol_repression))+ geom_point()+ geom_line(aes(group=1))+
+  geom_vline(aes(xintercept = year_month), data = ~ filter(cm_data,country_name=="Egypt",year %in% (2013:2016),ged_dummy_sb=="TRUE"),linetype="dotted")+
+  xlab("Year")+ ylab("Freq")+
+  ggtitle("Gov-Reb Non-Violent Repression")+
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(plot.title = element_text(color = "black", size=14, hjust=0.5),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black"),
+        axis.title.x = element_text(hjust=0.5, size=16),
+        axis.title.y= element_text(hjust=0.5,size=16),
+        axis.text = element_text(size=12,colour = "black")) +
+  scale_x_discrete(breaks = unique(cm_egy$year_month)[seq(1,48,6)])
+
+
+
+p<-grid.arrange(a,b,c,d, e, f,g,h,ncol=2,nrow=4)
+ggsave(p,filename=paste("Egypt_Escalation_Time_Series",".png", sep=""))
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
 
 
 
