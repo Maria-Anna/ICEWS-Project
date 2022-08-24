1fd872cd2e27720cd155261a4104292adc58e933
Maria-Anna Tsitsipa <maria.anna.tsitsipa@gmail.com>
2022-08-24 11:27:25 +0200

    commit

--- 4. OOS Evaluation and Forecasting/Linear_Estimates.R
+++ 4. OOS Evaluation and Forecasting/Linear_Estimates.R
@@ -3,68 +3,76 @@
 #Load necessary packages
 library(gtools)
 library(knitr)
+library(xtable)
 
 #Set working directory
+path_predictions<- "~/ICEWS-Project/3. Model/Predictions/"
+
 
 #------------------------------------------------------------------------------------------------------------------------------------------------#
 #For all ICEWS Variables
-#s=2
+#Lag: s=2
 
-#load try model 1
-load("Prediction_ICEWS/models/try_model_1.RData")
+#load Models
+load(paste0(path_predictions, "Prediction_ICEWS/models/try_model_1.RData"))
+load(paste0(path_predictions, "Prediction_ICEWS/models/try_model_2.RData"))
+load(paste0(path_predictions, "Prediction_ICEWS/models/try_model_2.RData"))
+
+#Estimates for all three stages
 summary_1 = summary(try_model_1)
 summary_1
-print(xtable(round(summary_1$p.table, digits = 4), type = "latex"), file = "Stage_1_estimates_2.tex")
-
-
-#load try model 2
-load("Prediction_ICEWS/models/try_model_2.RData")
 summary_2 = summary(try_model_2)
 summary_2
-print(xtable(round(summary_2$p.table, digits = 4), type = "latex"), file = "Stage_2_estimates_2.tex")
-
-
-#load try model 3
-load("Prediction_ICEWS/models/try_model_3.RData")
 summary_3 = summary(try_model_3)
 summary_3
-print(xtable(round(summary_3$p.table, digits = 4), type = "latex"), file = "Stage_3_estimates_2.tex")
 
 
-#s=7
+#LateX Table
+#print(xtable(round(summary_1$p.table, digits = 4), type = "latex"), file = "Stage_1_estimates_2.tex")
+#print(xtable(round(summary_2$p.table, digits = 4), type = "latex"), file = "Stage_2_estimates_2.tex")
+#print(xtable(round(summary_3$p.table, digits = 4), type = "latex"), file = "Stage_3_estimates_2.tex")
 
-#load try model 1
-load("Prediction_ICEWS/models_7/try_model_1.RData")
+
+#Lag: s=7
+
+#load Models
+load(paste0(path_predictions,"Prediction_ICEWS/models_7/try_model_1.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS/models_7/try_model_2.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS/models_7/try_model_3.RData"))
+
+
+#Estimates for all tree stages
 summary_1 = summary(try_model_1)
 summary_1
-print(xtable(round(summary_1$p.table, digits = 4), type = "latex"), file = "Stage_1_estimates_7.tex")
-
-
-#load try model 2
-load("Prediction_ICEWS/models_7/try_model_2.RData")
 summary_2 = summary(try_model_2)
 summary_2
-print(xtable(round(summary_2$p.table, digits = 4), type = "latex"), file = "Stage_2_estimates_7.tex")
-
-#load try model 3
-load("Prediction_ICEWS/models_7/try_model_3.RData")
 summary_3 = summary(try_model_3)
 summary_3
-print(xtable(round(summary_3$p.table, digits = 4), type = "latex"), file = "Stage_3_estimates_7.tex")
+
+
+#LateX Tables
+#print(xtable(round(summary_1$p.table, digits = 4), type = "latex"), file = "Stage_1_estimates_7.tex")
+#print(xtable(round(summary_2$p.table, digits = 4), type = "latex"), file = "Stage_2_estimates_7.tex")
+#print(xtable(round(summary_3$p.table, digits = 4), type = "latex"), file = "Stage_3_estimates_7.tex")
 
 
 #------------------------------------------------------------------------------------------------------------------------------------------------#
 #For Escalation Cov (no demands)
-#s=2
+#Lag: s=2
 
-load("Prediction_ICEWS_Esc/models/try_model_1_s2.RData")
-load("Prediction_ICEWS_Esc/models/try_model_2_s2.RData")
-load("Prediction_ICEWS_Esc/models/try_model_3_s2.RData")
+load(paste0(path_predictions,"Prediction_ICEWS_Esc/models/try_model_1_s2.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_Esc/models/try_model_2_s2.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_Esc/models/try_model_3_s2.RData"))
 
+#Estimates for all three Stages
 summary_1 = summary(try_model_1)
+summary_1
 summary_2 = summary(try_model_2)
+summary_2
 summary_3 = summary(try_model_3)
+summary_3
 
+#Extract significance 
 summary_1<-as.data.frame(summary_1$p.table)
 summary_1$Significance<-stars.pval(summary_1[,4])
 summary_2<-as.data.frame(summary_2$p.table)
@@ -72,20 +80,26 @@ summary_2$Significance<-stars.pval(summary_2[,4])
 summary_3<-as.data.frame(summary_3$p.table)
 summary_3$Significance<-stars.pval(summary_3[,4])
 
-xtable(summary_1, digits = 4)
-xtable(summary_2, digits = 4)
-xtable(summary_3, digits = 4)
+#LateX Table
+#xtable(summary_1, digits = 4)
+#xtable(summary_2, digits = 4)
+#xtable(summary_3, digits = 4)
 
 
-#s=7
-load("Prediction_ICEWS_Esc/models/try_model_1_s7.RData")
-load("Prediction_ICEWS_Esc/models/try_model_2_s7.RData")
-load("Prediction_ICEWS_Esc/models/try_model_3_s7.RData")
+#Lag: s=7
+load(paste0(path_predictions,"Prediction_ICEWS_Esc/models/try_model_1_s7.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_Esc/models/try_model_2_s7.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_Esc/models/try_model_3_s7.RData"))
 
+#Estimates for all tree stages
 summary_1 = summary(try_model_1)
+summary_1
 summary_2 = summary(try_model_2)
+summary_2
 summary_3 = summary(try_model_3)
+summary_3
 
+#Extract significance
 summary_1<-as.data.frame(summary_1$p.table)
 summary_1$Significance<-stars.pval(summary_1[,4])
 summary_2<-as.data.frame(summary_2$p.table)
@@ -93,23 +107,30 @@ summary_2$Significance<-stars.pval(summary_2[,4])
 summary_3<-as.data.frame(summary_3$p.table)
 summary_3$Significance<-stars.pval(summary_3[,4])
 
-xtable(summary_1, digits = 4)
-xtable(summary_2, digits = 4)
-xtable(summary_3, digits = 4)
+#LateX Table
+#xtable(summary_1, digits = 4)
+#xtable(summary_2, digits = 4)
+#xtable(summary_3, digits = 4)
 
 
 #------------------------------------------------------------------------------------------------------------------------------------------------#
 #For Escalation Cov with Cap Distance Interaction
-#s=2
+#Lag: s=2
 
-load("Prediction_ICEWS_cm_cap/models/try_model_1_s2.RData")
-load("Prediction_ICEWS_cm_cap/models/try_model_2_s2.RData")
-load("Prediction_ICEWS_cm_cap/models/try_model_3_s2.RData")
+#Load Datasets
+load(paste0(path_predictions,"Prediction_ICEWS_cm_cap/models/try_model_1_s2.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_cm_cap/models/try_model_2_s2.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_cm_cap/models/try_model_3_s2.RData"))
 
+#Estimates for all three Stages
 summary_1 = summary(try_model_1)
+summary_1
 summary_2 = summary(try_model_2)
+summary_2
 summary_3 = summary(try_model_3)
+summary_3
 
+#Extract significance
 summary_1<-as.data.frame(summary_1$p.table)
 summary_1$Significance<-stars.pval(summary_1[,4])
 summary_2<-as.data.frame(summary_2$p.table)
@@ -117,22 +138,56 @@ summary_2$Significance<-stars.pval(summary_2[,4])
 summary_3<-as.data.frame(summary_3$p.table)
 summary_3$Significance<-stars.pval(summary_3[,4])
 
-xtable(summary_1, digits = 4)
-xtable(summary_2, digits = 4)
-xtable(summary_3, digits = 4)
+#Latex Table
+#xtable(summary_1, digits = 4)
+#xtable(summary_2, digits = 4)
+#xtable(summary_3, digits = 4)
 
+
+#Lag: s=7
+
+load(paste0(path_predictions,"Prediction_ICEWS_cm_cap/models/try_model_1_s7.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_cm_cap/models/try_model_2_s7.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_cm_cap/models/try_model_3_s7.RData"))
+
+#Estimates for all tree stages
+summary_1 = summary(try_model_1)
+summary_1
+summary_2 = summary(try_model_2)
+summary_2
+summary_3 = summary(try_model_3)
+summary_3
+
+#Extract significance
+summary_1<-as.data.frame(summary_1$p.table)
+summary_1$Significance<-stars.pval(summary_1[,4])
+summary_2<-as.data.frame(summary_2$p.table)
+summary_2$Significance<-stars.pval(summary_2[,4])
+summary_3<-as.data.frame(summary_3$p.table)
+summary_3$Significance<-stars.pval(summary_3[,4])
+
+#Latex Table
+#xtable(summary_1, digits = 4)
+#xtable(summary_2, digits = 4)
+#xtable(summary_3, digits = 4)
 
 #------------------------------------------------------------------------------------------------------------------------------------------------#
 #For CM and PGM
+
 #s=2
-load("Prediction_ICEWS_CM_PGM/models/try_model_1_s2.RData")
-load("Prediction_ICEWS_CM_PGM/models/try_model_2_s2.RData")
-load("Prediction_ICEWS_CM_PGM/models/try_model_3_s2.RData")
+load(paste0("Prediction_ICEWS_CM_PGM/models/try_model_1_s2.RData"))
+load(paste0("Prediction_ICEWS_CM_PGM/models/try_model_2_s2.RData"))
+load(paste0("Prediction_ICEWS_CM_PGM/models/try_model_3_s2.RData"))
 
+#Estimates for all three stages
 summary_1 = summary(try_model_1)
+summary_1
 summary_2 = summary(try_model_2)
+summary_2
 summary_3 = summary(try_model_3)
+summary_3
 
+#Extract significance
 summary_1<-as.data.frame(summary_1$p.table)
 summary_1$Significance<-stars.pval(summary_1[,4])
 summary_2<-as.data.frame(summary_2$p.table)
@@ -140,22 +195,55 @@ summary_2$Significance<-stars.pval(summary_2[,4])
 summary_3<-as.data.frame(summary_3$p.table)
 summary_3$Significance<-stars.pval(summary_3[,4])
 
-xtable(summary_1, digits = 4)
-xtable(summary_2, digits = 4)
-xtable(summary_3, digits = 4)
+#LateX tables
+#xtable(summary_1, digits = 4)
+#xtable(summary_2, digits = 4)
+#xtable(summary_3, digits = 4)
 
+#Lag: s=7
+
+load(paste0(path_predictions,"Prediction_ICEWS_CM_PGM/models/try_model_1_s7.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_CM_PGM/models/try_model_2_s7.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_CM_PGM/models/try_model_3_s7.RData"))
+
+#Estimates for all tree stages
+summary_1 = summary(try_model_1)
+summary_1
+summary_2 = summary(try_model_2)
+summary_2
+summary_3 = summary(try_model_3)
+summary_3
+
+#Extract significance
+summary_1<-as.data.frame(summary_1$p.table)
+summary_1$Significance<-stars.pval(summary_1[,4])
+summary_2<-as.data.frame(summary_2$p.table)
+summary_2$Significance<-stars.pval(summary_2[,4])
+summary_3<-as.data.frame(summary_3$p.table)
+summary_3$Significance<-stars.pval(summary_3[,4])
+
+#Latex Table
+#xtable(summary_1, digits = 4)
+#xtable(summary_2, digits = 4)
+#xtable(summary_3, digits = 4)
 
 #------------------------------------------------------------------------------------------------------------------------------------------------#
 #For Low Level Violence Only
+
 #s=2
-load("Prediction_ICEWS_Low/models/try_model_1_s2.RData")
-load("Prediction_ICEWS_Low/models/try_model_2_s2.RData")
-load("Prediction_ICEWS_Low/models/try_model_3_s2.RData")
+load(paste0(path_predictions,"Prediction_ICEWS_Low/models/try_model_1_s2.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_Low/models/try_model_2_s2.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_Low/models/try_model_3_s2.RData"))
 
+#Estimates for all tree estimates
 summary_1 = summary(try_model_1)
+summary_1
 summary_2 = summary(try_model_2)
+summary_2
 summary_3 = summary(try_model_3)
+summary_3
 
+#Extract significance
 summary_1<-as.data.frame(summary_1$p.table)
 summary_1$Significance<-stars.pval(summary_1[,4])
 summary_2<-as.data.frame(summary_2$p.table)
@@ -163,21 +251,55 @@ summary_2$Significance<-stars.pval(summary_2[,4])
 summary_3<-as.data.frame(summary_3$p.table)
 summary_3$Significance<-stars.pval(summary_3[,4])
 
-xtable(summary_1, digits = 4)
-xtable(summary_2, digits = 4)
-xtable(summary_3, digits = 4)
+#Latex Table
+#xtable(summary_1, digits = 4)
+#xtable(summary_2, digits = 4)
+#xtable(summary_3, digits = 4)
+
+
+#s=7
+load(paste0(path_predictions,"Prediction_ICEWS_Low/models/try_model_1_s7.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_Low/models/try_model_2_s7.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_Low/models/try_model_3_s7.RData"))
+
+#Estimates for all tree estimates
+summary_1 = summary(try_model_1)
+summary_1
+summary_2 = summary(try_model_2)
+summary_2
+summary_3 = summary(try_model_3)
+summary_3
+
+#Extract significance
+summary_1<-as.data.frame(summary_1$p.table)
+summary_1$Significance<-stars.pval(summary_1[,4])
+summary_2<-as.data.frame(summary_2$p.table)
+summary_2$Significance<-stars.pval(summary_2[,4])
+summary_3<-as.data.frame(summary_3$p.table)
+summary_3$Significance<-stars.pval(summary_3[,4])
+
+#Latex Table
+#xtable(summary_1, digits = 4)
+#xtable(summary_2, digits = 4)
+#xtable(summary_3, digits = 4)
 
 #------------------------------------------------------------------------------------------------------------------------------------------------#
 #For CM and PGM without Cap Factor
+
 #s=2
-load("Prediction_ICEWS_CM_PGM_NO_INT/models/try_model_1_s2.RData")
-load("Prediction_ICEWS_CM_PGM_NO_INT/models/try_model_2_s2.RData")
-load("Prediction_ICEWS_CM_PGM_NO_INT/models/try_model_3_s2.RData")
+load(paste0(path_predictions,"Prediction_ICEWS_CM_PGM_NO_INT/models/try_model_1_s2.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_CM_PGM_NO_INT/models/try_model_2_s2.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_CM_PGM_NO_INT/models/try_model_3_s2.RData"))
 
+#Linear Estimates for all three stages     
 summary_1 = summary(try_model_1)
+summary_1
 summary_2 = summary(try_model_2)
+summary_2
 summary_3 = summary(try_model_3)
+summary_3
 
+#Extract significance
 summary_1<-as.data.frame(summary_1$p.table)
 summary_1$Significance<-stars.pval(summary_1[,4])
 summary_2<-as.data.frame(summary_2$p.table)
@@ -185,22 +307,26 @@ summary_2$Significance<-stars.pval(summary_2[,4])
 summary_3<-as.data.frame(summary_3$p.table)
 summary_3$Significance<-stars.pval(summary_3[,4])
 
-xtable(summary_1, digits = 4)
-xtable(summary_2, digits = 4)
-xtable(summary_3, digits = 4)
+#LateX Tables
+#xtable(summary_1, digits = 4)
+#xtable(summary_2, digits = 4)
+#xtable(summary_3, digits = 4)
 
 
 #------------------------------------------------------------------------------------------------------------------------------------------------#
 #For CM and PGM with PGM Cap Dist
-#s=2
-load("Prediction_ICEWS_CM_PGM_CAP_DIST/models/try_model_1_s2.RData")
-load("Prediction_ICEWS_CM_PGM_CAP_DIST/models/try_model_2_s2.RData")
-load("Prediction_ICEWS_CM_PGM_CAP_DIST/models/try_model_3_s2.RData")
 
+#s=2
+load(paste0(path_predictions,"Prediction_ICEWS_CM_PGM_CAP_DIST/models/try_model_1_s2.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_CM_PGM_CAP_DIST/models/try_model_2_s2.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_CM_PGM_CAP_DIST/models/try_model_3_s2.RData"))
+
+#Linear estimates for all tree stages
 summary_1 = summary(try_model_1)
 summary_2 = summary(try_model_2)
 summary_3 = summary(try_model_3)
 
+#Extract significance
 summary_1<-as.data.frame(summary_1$p.table)
 summary_1$Significance<-stars.pval(summary_1[,4])
 summary_2<-as.data.frame(summary_2$p.table)
@@ -208,44 +334,29 @@ summary_2$Significance<-stars.pval(summary_2[,4])
 summary_3<-as.data.frame(summary_3$p.table)
 summary_3$Significance<-stars.pval(summary_3[,4])
 
-xtable(summary_1, digits = 4)
-xtable(summary_2, digits = 4)
-xtable(summary_3, digits = 4)
-
-#------------------------------------------------------------------------------------------------------------------------------------------------#
-#For CM and PGM without Cap Factor
-#s=2
-load("Prediction_ICEWS_CM_PGM_NO_INT/models/try_model_1_s2.RData")
-load("Prediction_ICEWS_CM_PGM_NO_INT/models/try_model_2_s2.RData")
-load("Prediction_ICEWS_CM_PGM_NO_INT/models/try_model_3_s2.RData")
-
-summary_1 = summary(try_model_1)
-summary_2 = summary(try_model_2)
-summary_3 = summary(try_model_3)
-
-summary_1<-as.data.frame(summary_1$p.table)
-summary_1$Significance<-stars.pval(summary_1[,4])
-summary_2<-as.data.frame(summary_2$p.table)
-summary_2$Significance<-stars.pval(summary_2[,4])
-summary_3<-as.data.frame(summary_3$p.table)
-summary_3$Significance<-stars.pval(summary_3[,4])
-
-xtable(summary_1, digits = 4)
-xtable(summary_2, digits = 4)
-xtable(summary_3, digits = 4)
+#LateX Tables
+#xtable(summary_1, digits = 4)
+#xtable(summary_2, digits = 4)
+#xtable(summary_3, digits = 4)
 
 
 #------------------------------------------------------------------------------------------------------------------------------------------------#
 #For 3 Months Aggregation
-#s=2
-load("Prediction_ICEWS_Aggr/models/try_model_1_s2.RData")
-load("Prediction_ICEWS_Aggr/models/try_model_2_s2.RData")
-load("Prediction_ICEWS_Aggr/models/try_model_3_s2.RData")
 
+#Lag: s=2
+load(paste0(path_predictions,"Prediction_ICEWS_Aggr/models/try_model_1_s2.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_Aggr/models/try_model_2_s2.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_Aggr/models/try_model_3_s2.RData"))
+
+#Extract Linear Estimates for all tree stages
 summary_1 = summary(try_model_1)
+summary_1
 summary_2 = summary(try_model_2)
+summary_2
 summary_3 = summary(try_model_3)
+summary_3
 
+#Extract significance
 summary_1<-as.data.frame(summary_1$p.table)
 summary_1$Significance<-stars.pval(summary_1[,4])
 summary_2<-as.data.frame(summary_2$p.table)
@@ -253,21 +364,24 @@ summary_2$Significance<-stars.pval(summary_2[,4])
 summary_3<-as.data.frame(summary_3$p.table)
 summary_3$Significance<-stars.pval(summary_3[,4])
 
-xtable(summary_1, digits = 4)
-xtable(summary_2, digits = 4)
-xtable(summary_3, digits = 4)
+#LateX Tables
+#xtable(summary_1, digits = 4)
+#xtable(summary_2, digits = 4)
+#xtable(summary_3, digits = 4)
 
 #------------------------------------------------------------------------------------------------------------------------------------------------#
 #For 2 Months Aggregation
 #s=2
-load("Prediction_ICEWS_Aggr_2_months/models/try_model_1_s2.RData")
-load("Prediction_ICEWS_Aggr_2_months/models/try_model_2_s2.RData")
-load("Prediction_ICEWS_Aggr_2_months/models/try_model_3_s2.RData")
+load(paste0(path_predictions,"Prediction_ICEWS_Aggr_2_months/models/try_model_1_s2.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_Aggr_2_months/models/try_model_2_s2.RData"))
+load(paste0(path_predictions,"Prediction_ICEWS_Aggr_2_months/models/try_model_3_s2.RData"))
 
+#Linear Estimates for all tree stages
 summary_1 = summary(try_model_1)
 summary_2 = summary(try_model_2)
 summary_3 = summary(try_model_3)
 
+#Extract significance
 summary_1<-as.data.frame(summary_1$p.table)
 summary_1$Significance<-stars.pval(summary_1[,4])
 summary_2<-as.data.frame(summary_2$p.table)
@@ -275,10 +389,8 @@ summary_2$Significance<-stars.pval(summary_2[,4])
 summary_3<-as.data.frame(summary_3$p.table)
 summary_3$Significance<-stars.pval(summary_3[,4])
 
-xtable(summary_1, digits = 4)
-xtable(summary_2, digits = 4)
-xtable(summary_3, digits = 4)
-
-
-
+#LateX tables
+#xtable(summary_1, digits = 4)
+#xtable(summary_2, digits = 4)
+#xtable(summary_3, digits = 4)
 
